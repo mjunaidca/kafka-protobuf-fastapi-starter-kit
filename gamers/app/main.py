dcb31fa334e4b41c7437577313ced30536d0e585
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-05-19 13:36:34 +0500

    setup consumer using aiokafka

--- gamers/app/main.py
+++ gamers/app/main.py
@@ -1,6 +1,40 @@
 from fastapi import FastAPI
+from aiokafka import AIOKafkaConsumer
+import asyncio
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+KAFKA_BROKER = "broker:19092"
+KAFKA_TOPIC = "gamers"
+KAFKA_CONSUMER_GROUP_ID = "gamers-consumer-group"
+
+
+async def consume():
+    consumer = AIOKafkaConsumer(
+        KAFKA_TOPIC,
+        bootstrap_servers=KAFKA_BROKER,
+        group_id=KAFKA_CONSUMER_GROUP_ID)
+    # Get cluster layout and join group `my-group`
+    await consumer.start()
+    try:
+        # Consume messages
+        async for msg in consumer:
+            print("consumed: ", msg.topic, msg.partition, msg.offset,
+                  msg.key, msg.value, msg.timestamp)
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("startup")
+    asyncio.create_task(consume())
+    print("Created Task")
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
 
 @app.get("/")
 def hello():
