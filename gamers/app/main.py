ce1a61ac0d6f924166310da1eeee6c7fcfd77ad0
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-05-19 14:16:12 +0500

    add logger to replace print

--- gamers/app/main.py
+++ gamers/app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from aiokafka import AIOKafkaConsumer
 import asyncio
 from contextlib import asynccontextmanager
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 KAFKA_BROKER = "broker:19092"
 KAFKA_TOPIC = "gamers"
@@ -11,29 +14,28 @@ KAFKA_CONSUMER_GROUP_ID = "gamers-consumer-group"
 async def consume():
     consumer = AIOKafkaConsumer(
         KAFKA_TOPIC,
-        bootstrap_servers=KAFKA_BROKER,
-        group_id=KAFKA_CONSUMER_GROUP_ID)
-    # Get cluster layout and join group `my-group`
+        bootstrap_servers=KAFKA_BROKER
+    )
     await consumer.start()
     try:
-        # Consume messages
         async for msg in consumer:
-            print("consumed: ", msg.topic, msg.partition, msg.offset,
-                  msg.key, msg.value, msg.timestamp)
+            logging.info(
+                "{}:{:d}:{:d}: key={} value={} timestamp_ms={}".format(
+                    msg.topic, msg.partition, msg.offset, msg.key, msg.value,
+                    msg.timestamp)
+            )
     finally:
-        # Will leave consumer group; perform autocommit if enabled.
         await consumer.stop()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("startup")
+    print("Starting consumer")
     asyncio.create_task(consume())
-    print("Created Task")
     yield
-
+    print("Stopping consumer")
 
 app = FastAPI(lifespan=lifespan)
-
 
 
 @app.get("/")
