ef5d9d7f3828830b8783bc7d72791b2c1edb3f07
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-05-26 12:29:44 +0500

    feat: Add protobuf support for gamers registration
    This commit adds support for protobuf serialization and deserialization for the gamers registration feature. It includes the following changes:
    - Added gamers.proto file to define the GamePlayers message
    - Added gamers_pb2.py file generated by the protocol buffer compiler
    - Updated Dockerfile.dev to install protobuf-compiler
    - Updated main.py to deserialize incoming messages using protobuf
    - Updated register_new_player function to serialize player data using protobuf

--- gamers/app/main.py
+++ gamers/app/main.py
@@ -5,11 +5,14 @@ from contextlib import asynccontextmanager
 from sqlmodel import SQLModel
 import logging
 
+from app import gamers_pb2
+
 logging.basicConfig(level=logging.INFO)
 
 KAFKA_BROKER = "broker:19092"
 KAFKA_TOPIC = "gamers"
 KAFKA_CONSUMER_GROUP_ID = "gamers-consumer-group"
+
 
 class GamePlayersRegistration(SQLModel):
     player_name: str
@@ -22,17 +25,19 @@ async def consume():
     # Milestone: CONSUMER INTIALIZE
     consumer = AIOKafkaConsumer(
         KAFKA_TOPIC,
-        bootstrap_servers=KAFKA_BROKER
+        bootstrap_servers=KAFKA_BROKER,
+        auto_offset_reset="earliest"
+
     )
 
     await consumer.start()
     try:
         async for msg in consumer:
-            logging.info(
-                "{}:{:d}:{:d}: key={} value={} timestamp_ms={}".format(
-                    msg.topic, msg.partition, msg.offset, msg.key, msg.value,
-                    msg.timestamp)
-            )
+            logging.info("RAW MESSAGE: %s", msg.value)
+            deserialize_message = gamers_pb2.GamePlayers()
+            deserialize_message.ParseFromString(msg.value)
+            logging.info("DERSERLIZSEA: %s", deserialize_message)
+
     finally:
         await consumer.stop()
 
@@ -56,11 +61,18 @@ def hello():
 async def register_new_player(player_data: GamePlayersRegistration):
     producer = AIOKafkaProducer(bootstrap_servers=KAFKA_BROKER)
 
+    player_data_prot = gamers_pb2.GamePlayers(
+        player_name=player_data.player_name, age=player_data.age, email=player_data.email, phone_number=player_data.phone_number)
+    print("player_data_prot", player_data_prot)
+
+    player_data_prot_serialized = player_data_prot.SerializeToString()
+    print("player_data_prot_serialized", player_data_prot_serialized)
+
     await producer.start()
 
     try:
-        await producer.send_and_wait(KAFKA_TOPIC, player_data.model_dump_json().encode('utf-8'))
+        await producer.send_and_wait(KAFKA_TOPIC, player_data_prot_serialized)
     finally:
         await producer.stop()
 
-    return player_data.model_dump_json() 
+    return player_data.model_dump_json()
